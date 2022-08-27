5a5ec2aea68bd8d5a7be280d89f55e7f441e2b53
Paulina Almada Martinez <111139667+A01710029@users.noreply.github.com>
2022-08-26 21:36:07 -0500

    Add files via upload

--- programa_final.py
+++ programa_final.py
@@ -2,3 +2,27 @@
 #a escribir mis comentarios en inglés. ¿Le molesta si comento
 #mi proyecto final en inglés, o quiere que lo haga en español?
 
+#import math and random
+import math
+import random
+
+#greet user
+
+#ask for top number
+print("¿Cuál debería ser el número tope?")
+tope = int(input())
+
+#generate random number
+num_compu = random.randint(0,tope)
+
+#prompt user to start guessing
+adivinanza = int(input())
+
+#compare user guess to random number
+if adivinanza < num_compu:
+    print("Intenta un número más pequeño")
+elif adivinanza > num_compu:
+    print("Intenta un número más grande")
+
+intentos = int()
+intentos = intentos + 1
