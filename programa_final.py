8c356c7865ff49f21b367423b5a6fe019a949479
Paulina Almada Martinez <111139667+A01710029@users.noreply.github.com>
2022-08-26 19:59:17 -0500

    Add files via upload

---
+++ programa_final.py
@@ -0,0 +1,4 @@
+#Profesor, como nota personal, aprendí a programar en inglés y estoy acostumbrada
+#a escribir mis comentarios en inglés. ¿Le molesta si comento
+#mi proyecto final en inglés, o quiere que lo haga en español?
+
