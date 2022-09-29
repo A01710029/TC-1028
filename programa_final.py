74c849ea09d9660b5bfcab230583963de8b507a3
Paulina Almada Martinez <111139667+A01710029@users.noreply.github.com>
2022-09-29 09:17:41 -0500

    Add files via upload

--- programa_final.py
+++ programa_final.py
@@ -1,7 +1,7 @@
 #Estoy escribiendo los comentarios en inglés. Hablé con el 
 #profesor sobre esta decisión.
 
-#import math and random
+#import math, random, and time
 import math
 import random
 
@@ -9,6 +9,7 @@ import random
 nombre = str(input("¿Cómo te llamas?\n"))
 adivinanza = int()
 intentos = 1
+lista_intentos = []
 
 #ask for top number
 print("¿Cuál debería ser el número tope?")
@@ -27,21 +28,39 @@ def comienzo(nombre, tope):
     print("Hola", nombre)
     print("""¡Intenta adivinar en qué número 
 entre 1 y""", str(tope) + """ estoy pensando!""")
+    print("Escribe '0' si quieres ver tus intentos anteriores")
 
 #function to prompt, analyze, give feedback on, 
 #and keep count of guesses
+#add failed guesses to list
 def adivina_compara(adivinanza, intentos):
     while (adivinanza != num_compu):
         adivinanza = int(input())
-        if adivinanza < num_compu:
+        if adivinanza == 0:
+            intentos = intentos - 1 #removes calls to list from counter
+            imprimir_lista()
+        if adivinanza < num_compu: #guess is less than target num
             intentos = intentos + 1
+            lista_intentos.append(adivinanza)
             print("Intenta un número más grande")
-        elif adivinanza > num_compu:
+        elif adivinanza > num_compu: #guess is more than target num
             intentos = intentos + 1
+            lista_intentos.append(adivinanza)
             print("Intenta un número más pequeño")
     print("¡Adivinaste el número,", nombre + "!")
     intentos = str(intentos)
     return "¿Cuántos intentos tomo?: " + intentos
+
+#function to print failed guesses list
+#(has to be activated by user)
+def imprimir_lista():
+    i = 0
+    len_intentos = len(lista_intentos)
+    while(i < len_intentos-1):
+        if(lista_intentos[i] == 0):
+            lista_intentos.remove(0) #removes calls to list from list
+        i = i + 1
+    print(lista_intentos)
 
 #call function + print final results
 comienzo(nombre, tope)
