9612f50a01354153b0d6ac2ffbcbcac34f54cac5
Paulina Almada Martinez <111139667+A01710029@users.noreply.github.com>
2022-10-13 23:58:30 -0500

    Add files via upload

--- programa_final.py
+++ programa_final.py
@@ -1,106 +1,151 @@
-#Estoy escribiendo los comentarios en inglés. Hablé con el 
-#profesor sobre esta decisión.
+"""
+El programa genera un número aleatorio basado en un 
+parametro del usuario y compara sus adivinanzas
+hasta que lo adivina correctamente. 
+Permite que se lleven a cabo varias rondas hasta que, 
+finalmente, el usuario decide terminar el juego 
+y regresa las mejor ronda.
+"""
 
-#import math, random, and time
-import math
-import random
-import time
+#bibliotecas 
+import math 
+import random 
+import time 
 
-#create matrix to store each round values
-#(name, top number, attempts, and total time)
-matriz_rondas = []
+"""
+==== variables del juego (no cambian cada ronda) ==== 
+"""
 
-#starts counter for multiple-round name readjustment
-c = 1
+#matriz para guardar valores de cada ronda 
+matriz_rondas = [] 
+
+#contador de rondas 
+rondas = 1 
+
+#empieza ciclo del juego 
 while True: 
-    #declare variables
-    nombre = str(input("¿Cómo te llamas? \n"))
-    adivinanza = int()
-    intentos = 1
-    lista_intentos = []
-    lista_ronda = []
+    
+    """
+    ==== variables del juego (cambian cada ronda) ==== 
+    """
 
-    #ask user for + set top number 
-    print("¿Cuál debería ser el número tope?")
-    tope = int(input())
+    nombre = str(input("¿Cómo te llamas? \n")) 
+    adivinanza = int() 
+    intentos = 1 
+    lista_intentos = [] 
+    lista_ronda = [] 
 
-    #check that top number is not negative or smaller than 2
-    #once ok, generate random number
-    while(tope <= 1):
-            print("El número tiene que ser mayor que uno.")
-            print("¿Cuál debería ser el número tope?")
-            tope = int(input())
-    num_compu = random.randint(1,tope)
+    #declara parametro para número aleatorio 
+    print("¿Cuál debería ser el número tope?") 
+    tope = int(input()) 
+
+    """
+    verifica que el número tope no sea negativo o menor a 2 
+    cuando se recibe un parametro aceptable, se genera el 
+    número aleatorio (num_compu) 
+    """
+    while(tope <= 1): 
+            print("El número tiene que ser mayor que uno.") 
+            print("¿Cuál debería ser el número tope?") 
+            tope = int(input()) 
+    num_compu = random.randint(1,tope) 
         
-    #function to display game start text
-    def comienzo(nombre, tope):
-        lista_ronda.append(nombre)
-        lista_ronda.append(tope)
-        print("Hola", nombre)
+    """
+    ==== funciones del juego ==== 
+    """
+
+    def comienzo(nombre, tope): 
+        """
+        recibe: nombre valor string, tope valor numérico 
+        agrega valores a la lista de la ronda actual 
+        imprime texto para introducir la ronda 
+        """
+        lista_ronda.append(nombre) 
+        lista_ronda.append(tope) 
+        print("¡Hola", nombre + "!") 
         print("""¡Intenta adivinar en qué número 
-entre 1 y""", str(tope) + """ estoy pensando!""")
-        print("Escribe '0' si quieres ver tus intentos anteriores")
+entre 1 y""", str(tope) + """ estoy pensando!""") 
+        print("Escribe '0' si quieres ver tus intentos anteriores.") 
 
-    #function to prompt, analyze, give feedback on, 
-    #and keep count of guesses
-    #also adds failed guesses to list
     def adivina_compara(adivinanza, intentos):
-        while (adivinanza != num_compu):
-            adivinanza = int(input())
-            if adivinanza == 0:
-                intentos = intentos - 1 #removes calls to list from counter
-                imprimir_lista()
-            if adivinanza < num_compu: #guess is less than target num
-                intentos = intentos + 1
-                lista_intentos.append(adivinanza)
-                print("Intenta un número más grande")
-            elif adivinanza > num_compu: #guess is more than target num
-                intentos = intentos + 1
-                lista_intentos.append(adivinanza)
-                print("Intenta un número más pequeño")
-        print("¡Adivinaste el número,", nombre + "!")
-        lista_ronda.append(intentos)
-        return "¿Cuántos intentos tomó?: " + str(intentos)
+        """
+        recibe: adivinanza valor numérico, intentos valor numérico 
+        compara adivinanza al número aleatorio, da pistas al usuario 
+        basado en si es mayor o menor el valor, y agrega 
+        adivinanzas a la lista de la ronda 
+        también agrega al valor de intentos cada vez que se 
+        compara una adivinanza (contador del ciclo) 
+        y permite activar función para imprimir intentos anteriores 
+        devuelve: cantidad de intentos (valor entero del contador) 
+        """
+        while (adivinanza != num_compu): 
+            adivinanza = int(input()) 
+            if adivinanza == 0: 
+                intentos = intentos - 1
+            if adivinanza < num_compu: 
+                intentos = intentos + 1 
+                lista_intentos.append(adivinanza) 
+                print("Intenta un número más grande") 
+            elif adivinanza > num_compu: 
+                intentos = intentos + 1 
+                lista_intentos.append(adivinanza) 
+                print("Intenta un número más pequeño") 
+        print("¡Adivinaste el número,", nombre + "!") 
+        lista_ronda.append(intentos) 
+        return "¿Cuántos intentos tomó?: " + str(intentos) 
 
-    #function to print failed guesses list
-    #(has to be activated by user)
     def imprimir_lista():
-        i = 0
-        len_intentos = len(lista_intentos)
-        while(i < len_intentos-1):
-            if(lista_intentos[i] == 0):
-                lista_intentos.remove(0) #removes calls to list (0 values) from list
-            i = i + 1
-        print(lista_intentos)
+        """
+        función auxiliar para imprimir 
+        muestra en pantalla los intentos previos (fallados) 
+        en forma de lista 
+        """
+        i = 0 
+        len_intentos = len(lista_intentos) 
+        while(i < len_intentos-1): 
+            if(lista_intentos[i] == 0): 
+                lista_intentos.remove(0) 
+            i = i + 1 
+        print(lista_intentos) 
 
-    #calculates the average of each round (guesses and time)
-    #then prints the name of the "best" round
     def mejor_adivinanza(matriz):
-        menor = matriz[0][2]/matriz[0][1]
+        """
+        recibe: matriz (valores de todas las rondas) 
+        divide cada adivinanza por el valor tope y compara 
+        los promedios de todas las rondas para identificar el valor menor 
+        si hay un empate, nombre la última ronda con el valor menor
+        devuelve: el nombre de la ronda con el menor promedio 
+        """
+        menor_val = matriz[0][2]/matriz[0][1]
         menor_nom = ""
         i = 0
         while i < len(matriz):
             j = 0
             while j < len(matriz[0]):
-                if(matriz[i][2]/matriz[i][1] < menor):
-                    menor = matriz[i][2]
+                if(matriz[i][2]/matriz[i][1] < menor_val):
+                    menor_val = matriz[i][2]
                     menor_nom = matriz[i][0]
                 else:
                     menor_nom = matriz[0][0]
                 j = j + 1
             i = i + 1
         return menor_nom 
-
-    #returns the name of the round with the lowest time
+    
     def mejor_tiempo(matriz):
-        menor = matriz[0][3]
+        """
+        recibe: matriz (valores de todas las rondas) 
+        compara los valores de tiempo de todas las rondas 
+        para identificar el valor menor 
+        devuelve: el nombre de la ronda con el menor tiempo 
+        """
+        menor_val = matriz[0][3]
         menor_nom = ""
         i = 0
         while i < len(matriz):
             j = 0
             while j < len(matriz[0]):
-                if(matriz[i][3] < menor):
-                    menor = matriz[i][2]
+                if(matriz[i][3] < menor_val):
+                    menor_val = matriz[i][2]
                     menor_nom = matriz[i][0]
                 else:
                     menor_nom = matriz[0][0]
@@ -108,20 +153,26 @@ entre 1 y""", str(tope) + """ estoy pensando!""")
             i = i + 1
         return menor_nom
 
-    #returns the best round based on average of guesses/top number
-    #and shortest time
     def stats_rondas(matriz):
+        """
+        recibe: matriz (valores de todas las rondas) 
+        llama las funciones que comparan los valores entre rondas
+        para encontrar las mejores rondas
+        devuelve: los nombres de las mejores rondas, ordenadas
+        """
         print(matriz_rondas)
         mejor_a = mejor_adivinanza(matriz) 
         mejor_t = mejor_tiempo(matriz)
         return "La ronda con el mejor promedio de adivinanzas fue: " + mejor_a \
             + "\nLa ronda con el mejor tiempo fue: " + mejor_t
 
-    #replaces the original inputted name 
-    #with a name + round number
-    #for repeated names
-    #(assumes user is playing against themselves)
     def nombre_replace(nombre, rep):
+        """"
+        recibe: nombre valor string y rep valor string
+        remplaza el nombre de la ronda actual con una versión
+        del mismo nombre que incluye el número de ronda
+        para poder diferenciar las rondas de un mismo usuario
+        """
         i = 1
         while i < len(matriz_rondas):
             j = 0
@@ -130,39 +181,38 @@ entre 1 y""", str(tope) + """ estoy pensando!""")
                     matriz_rondas[i][j] = rep
                 j = j + 1
             i = i + 1
-        return ""
-    
-    #call functions for each round
-    #activates, ends + prints timer
+
+    """
+    ==== parte principal del programa ====
+    """
     comienzo(nombre, tope)
-    start_time = time.time()
+    tiempo_inicial = time.time()
     print(adivina_compara(adivinanza, intentos))
-    end_time = time.time()
-    total_time = round(end_time-start_time,2)
-    lista_ronda.append(total_time)
+    tiempo_final = time.time()
+    tiempo_total = round(tiempo_final - tiempo_inicial, 2)
+    lista_ronda.append(tiempo_total)
     matriz_rondas.append(lista_ronda)
-    print("¿Cuánto tiempo tomó?:", total_time, "secs")
+    print("¿Cuánto tiempo tomó?:", tiempo_total, "secs")
+   
+    """
+    verifica si el nombre de la ronda actual se ha repetido
+    si es verdad, llama la función para agregarle
+    número de ronda al nombre (para diferenciarlos)
+    """
+    if(rondas > 1): 
+        nombre_repeat = nombre + str(rondas)
+        nombre_replace(nombre, nombre_repeat)
 
-    #checks if user name has been repeated
-    if(c > 1): 
-        i = 0
-        while i < len(matriz_rondas):
-            j = 0
-            while j < len(matriz_rondas[0]):
-                if(matriz_rondas[0][i] == nombre):
-                    nombre_repeat = nombre + str(c)
-                    nombre_replace(nombre, nombre_repeat)
-                j = j + 1
-            i = i + 1
+    #agrega al contador de rondas 
+    rondas = rondas + 1
 
-    #adds to counter for multiple-round name adjustment
-    c = c + 1
-
-    #asks user if they'd like to play again
-    #if yes, restarts the loop / activates another round
+    """
+    activa más rondas (repite el ciclo)
+    o termina el juego (termina el ciclo) e imprime resultados finales
+    """
     check = input("¿Quieres jugar de nuevo? Escribe y o n \n")
     if(check == "y"):
         continue
     else:
-        print(stats_rondas(matriz_rondas)) #final results of the game
+        print(stats_rondas(matriz_rondas))
     break
